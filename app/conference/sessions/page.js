45ec7111ec18c667a59a8ee915acd403d421d87f
Blain Noone <nooneblain@gmail.com>
2023-07-31 15:41:39 +0100

    Next.js app Version 1

---
+++ app/conference/sessions/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+      <>
+        <h1>Welcome to Speaker sessions</h1>
+        <h2><Link href="/conference">Back to Conferences</Link></h2>
+      </>
+    );
+  }
+  
--- app/settings/page.js
+++ app/settings/page.js
@@ -1,9 +1,7 @@
-import Link from "next/link";
-
 export default function Page() {
   return (
     <>
-      <h1>Welcome to App page</h1>
+      <h1>Welcome to Settings</h1>
     </>
   );
 }
